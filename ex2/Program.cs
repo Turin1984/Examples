6ec8739dc4e257673a054d4495db455742adbe8b
Turin1984 <turin84@mail.ru>
2022-06-05 16:56:04 +0300

    Ввод имени и вывод на экран

---
+++ ex2/Program.cs
@@ -0,0 +1,5 @@
+﻿// Ввести имя пользователя и вывести его на экран
+Console.WriteLine ("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
